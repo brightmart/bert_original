42fed772a1c3f8186405f04de426422ac10d6567
b <brightmart@hotmail.com>
2020-02-06 22:53:54 +0800

    update

--- create_pretrain_data_c5_bertbase.sh
+++ create_pretrain_data_c5_bertbase.sh
@@ -15,10 +15,10 @@ do
   echo $l
   python3 create_pretraining_data.py  --input_file=gs://clue_pretrain_corpus/raw_txt_corpus/train/clue_pretrain_$l.txt \
     --output_file=gs://clue_pretrain_corpus/tfrecords/bert_base_128/clue_pretrain128_$i.tfrecord --vocab_file=./resources/vocab.txt \
-    --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5 >$PWD/log/tfrecord_$i.log 2>&1 &
+    --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5 >$PWD/log_c5/tfrecord_$i.log 2>&1 &
 
   j=$[j+1]
-  if [ $j -eq 72 ];then
+  if [ $j -eq 7 ];then
     wait
     j=0
   fi
