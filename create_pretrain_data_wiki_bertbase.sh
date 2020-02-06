de28f7e76509ba8aa088a6afe0478cb6daddf5e9
b <brightmart@hotmail.com>
2020-02-06 23:07:50 +0800

    update

--- create_pretrain_data_wiki_bertbase.sh
+++ create_pretrain_data_wiki_bertbase.sh
@@ -15,7 +15,7 @@ do
   echo $l
   python3 create_pretraining_data.py  --input_file=gs://raw_text/wiki_all_raw/wiki_"$i"_string.txt_new.txt \
     --output_file=gs://clue_pretrain_corpus/tfrecords/bert_base_wiki_128/wiki_$i.tfrecord --vocab_file=./resources/vocab.txt \
-    --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5 >$PWD/log_c5/tfrecord_$i.log 2>&1 &
+    --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5 >$PWD/log_wiki/tfrecord_$i.log 2>&1 &
 
   j=$[j+1]
   if [ $j -eq 7 ];then
